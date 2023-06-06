053c38e98cf0466e8a8f5e2373c3c6feef5ee372
Hamilton Carlos da Silva Santos <santos.c.hamilton@gmail.com>
2023-06-05 21:10:31 -0300

    first commit, project creation

---
+++ AcademiApp/AcademiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademiAppApp.swift
+//  AcademiApp
+//
+//  Created by Hamilton Carlos da Silva Santos on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AcademiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
